185e655c576764952968be71dd94c3be3075f940
Martin Higgs <martinhiggs85@googlemail.com>
2015-08-12 20:04:12 +0100

    Removed template system UI controls in favour of minimal KitKat UI implementation.

--- app/src/main/java/io/mh175/mancala/GameActivity.java
+++ app/src/main/java/io/mh175/mancala/GameActivity.java
@@ -2,16 +2,35 @@ package io.mh175.mancala;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends Activity {
 
     /**
-     * Called on activity creation
-     * @param savedInstanceState Bundle saved from a previous onDestroy
+     * Setup view on activity creation.
+     * @param savedInstanceState Bundle saved from a previous onDestroy.
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    /**
+     * Handler called when window focus changes to or away from this activity.
+     * @param hasFocus Whether this boolean has received focus or not.
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        // If we got focus, set KitKat style fullscreen immersion for the game.
+        if (hasFocus) {
+            this.getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 }
